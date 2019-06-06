98400c5ae4acde7a934bfe5436ca532f2c981f07
Jan Okraska <jan.okraska@cern.ch>
2019-06-06 15:11:27 +0200

    cluster: bump reana-commons version
    Signed-off-by: Jan Okraska <jan.okraska@cern.ch>

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ install_requires = [
     'jsonschema[format]>=2.6.0,<2.7',
     'kubernetes>=9.0.0',
     'PyYAML>=5.1',
-    'reana-commons>=0.5.0,<0.6.0',
+    'reana-commons>=0.6.0.dev20190604,<0.7.0',
     'tablib>=0.12.1,<0.13',
     'urllib3==1.24.1',
 ]
